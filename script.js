ae9f8ae4db24fd52f54962a2b656c65c85c38dbe
laurabasaraba <105298368+laurabasaraba@users.noreply.github.com>
2024-02-10 11:59:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openArticles() {
+    window.location.href="idex.html"
+}
+
+function openContact() {
+    window.location.href="contact.html"
+}
+
+function openReviwes() {
+    window.location.href="reviews.html"
+}
